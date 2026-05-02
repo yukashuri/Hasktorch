490d3628b830fb5c351400513edb3f75a7522e74
Yuka Kobayashi <youseichan1133@gmail.com>
2026-05-02 05:55:09 +0000

    Session3

---
+++ Session3/LinerRegression.hs
@@ -0,0 +1,56 @@
+import Torch.Tensor (Tensor, asTensor)
+import Torch.Functional (add, mul)
+
+ys :: Tensor
+ys = asTensor ([130, 195, 218, 166, 163, 155, 204, 270, 205, 127, 260, 249, 251, 158, 167] :: [Float])
+xs :: Tensor
+xs = asTensor ([148, 186, 279, 179, 216, 127, 152, 196, 126, 78, 211, 259, 255, 115, 173] :: [Float])
+
+-- 予測値を返す
+linear :: 
+    (Tensor, Tensor) -> -- ^ parameters ([a, b]: 1 × 2, c: scalar)
+    Tensor ->           -- ^ data x: 1 × 10
+    Tensor              -- ^ z: 1 × 10
+linear (slope, intercept) input = 
+    let y = mul slope input
+    in add y intercept
+
+-- -- 予測値がどれくらいずれているか計算する関数
+-- mseLoss :: Tensor -> Tensor -> Tensor
+-- mseLoss y_pre y = 
+--     let diff = sub y_pre y
+--         squared = mul diff diff         -- 要素ごとの掛け算
+--     in  torch.mean(squared)
+
+-- -- パラメータを更新する関数
+-- updateStep :: Float -> (Tensor, Tensor) -> Int -> (Tensor, Tensor)
+-- updateStep learningRate (a, b) n = 
+
+-- -- 指定した回数だけupdateStepを繰り返し適用する関数
+-- train :: Int -> Float -> (Tensor, Tensor)
+
+
+main :: IO ()
+main = do
+  -- Below are pseudo code
+  let sampleA = asTensor (0.555 :: Float)
+  let sampleB = asTensor (94.585026 :: Float)
+	
+  -- Iterate through the provided xs and ys data. 
+  -- For each pair, convert x to a tensor, calculate the estimatedY using your linear function with the provided sampleA and sampleB, and print both the correct y and the estimatedY.
+  
+  let estimatedYs = linear (sampleA, sampleB) xs
+
+  putStrLn $ "correct answer: "
+  print ys
+
+  putStrLn $ "estimated: "
+  print estimatedYs
+    
+    -- Expected outputs:
+    -- correct answer: 148
+    -- estimated: ?
+    -- *******
+    -- correct answer: 186
+    -- ...
+  return ()
